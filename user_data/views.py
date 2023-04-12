e26e3c64fc944d72751b00d4f0601a229a47f76b
Mohammad Nimrawi <mohammad.alnimrawi@makane.com>
2023-04-12 05:00:51 +0300

    enhance user data

--- user_data/views.py
+++ user_data/views.py
@@ -6,8 +6,14 @@ from django.core.serializers.json import DjangoJSONEncoder
 
 # Create your views here.
 def get_user_data(request):
-    country = request.META.get("HTTP_X_VERCEL_IP_COUNTRY")
-    country_name = pycountry.countries.get(alpha_2=country).name +  pycountry.countries.get(alpha_2=country).flag if country else None
-    timezone = request.META.get("HTTP_X_VERCEL_IP_TIMEZONE")
-    return JsonResponse({"country_name": country_name,
-                        "timezone": timezone}, safe=False)
+    keys_to_get = [
+        "HTTP_X_VERCEL_IP_COUNTRY",
+        "HTTP_X_VERCEL_IP_TIMEZONE"
+    ]
+    values = {key: request.META.get(key) for key in keys_to_get}
+
+    country = pycountry.countries.get(alpha_2=values["HTTP_X_VERCEL_IP_COUNTRY"])
+
+    return JsonResponse({"country_name": country.name,
+                        "country_flag": country.flag,
+                        "timezone": values["HTTP_X_VERCEL_IP_TIMEZONE"]}, safe=False)
