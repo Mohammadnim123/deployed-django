14559b9027bcb504e250047a74e918d32372da4b
Mohammad Nimrawi <mohammad.alnimrawi@makane.com>
2023-04-12 04:28:40 +0300

    vercel config

--- user_data/views.py
+++ user_data/views.py
@@ -1,3 +1,13 @@
+import json
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.core.serializers.json import DjangoJSONEncoder
 
 # Create your views here.
+def get_user_data(request):
+    class CustomEncoder(DjangoJSONEncoder):
+        def default(self, obj):
+            return str(obj)
+
+    meta_data = json.loads(json.dumps(request.META, cls=CustomEncoder))
+    return JsonResponse(meta_data, safe=False)
