7a3d01e5cae54750c3b25a8dce32385d630b614e
Mohammad Nimrawi <mohammad.alnimrawi@makane.com>
2023-04-12 04:50:28 +0300

    Get country from request

--- practice/settings.py
+++ practice/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1skp%m0q6js)=erm1c782lv=#-0%of5uqt^n*t+oi!vz^34rgr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = [".vercel.app", "*"]
 
 
 # Application definition
--- user_data/views.py
+++ user_data/views.py
@@ -1,13 +1,13 @@
-import json
+from datetime import datetime
+import pycountry
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.core.serializers.json import DjangoJSONEncoder
 
 # Create your views here.
 def get_user_data(request):
-    class CustomEncoder(DjangoJSONEncoder):
-        def default(self, obj):
-            return str(obj)
-
-    meta_data = json.loads(json.dumps(request.META, cls=CustomEncoder))
-    return JsonResponse(meta_data, safe=False)
+    country = request.META.get("HTTP_X_VERCEL_IP_COUNTRY")
+    country_name = pycountry.countries.get(alpha_2=country).name +  pycountry.countries.get(alpha_2=country).flag if country else None
+    timezone = request.META.get("HTTP_X_VERCEL_IP_TIMEZONE")
+    return JsonResponse({"country_name": country_name,
+                        "timezone": timezone}, safe=False)
